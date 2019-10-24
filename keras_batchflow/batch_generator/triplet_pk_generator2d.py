d6b5f552861d0e8dc5e78b5b42237f379324ba60
maxsch3 <maxim.scherbak@gmail.com>
2019-10-24 06:04:54 +0100

    #4 lead column is now randomly selected. Tests updated and passed

--- keras_batchflow/batch_generator/triplet_pk_generator2d.py
+++ keras_batchflow/batch_generator/triplet_pk_generator2d.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from .triplet_pk_generator import TripletPKGenerator
 
 
@@ -17,15 +18,24 @@ class TripletPKGenerator2D(TripletPKGenerator):
         """
         This method is the core of triplet PK batch generator
         """
-        classes_selected = self.class_ref.sample(self.classes_in_batch)
+        swap = np.random.binomial(1, 0.5, 1)
+        if swap:
+            triplet_labels = self.triplet_label[::-1]
+            # sample along columns of crosstab
+            classes_selected = self.class_ref.T.sample(self.classes_in_batch)
+        else:
+            triplet_labels = self.triplet_label
+            classes_selected = self.class_ref.sample(self.classes_in_batch)
         classes1_selected = classes_selected.index.values
         classes2_selected = classes_selected.loc[:, classes_selected.sum() > 0].columns.values
-        batch = self.data.loc[self.data[self.triplet_label[0]].isin(classes1_selected), :].\
-            groupby(self.triplet_label[0]).apply(lambda x: self._select_samples_for_class(x, classes2_selected))
+        batch = self.data.loc[self.data[triplet_labels[0]].isin(classes1_selected), :].\
+            groupby(triplet_labels[0]).apply(lambda x: self._select_samples_for_class(x,
+                                                                                      classes2_selected,
+                                                                                      triplet_labels[1]))
         return batch
 
-    def _select_samples_for_class(self, df, classes2_selected):
-        batch = df.loc[df[self.triplet_label[1]].isin(classes2_selected), :]
+    def _select_samples_for_class(self, df, classes2_selected, triplet_label):
+        batch = df.loc[df[triplet_label].isin(classes2_selected), :]
         if batch.shape[0] <= self.sample_per_class:
             return batch
         return batch.sample(self.sample_per_class, replace=False)
