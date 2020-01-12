33497bd2b03243234fcb9312dcea8b714484dd36
maxsch3 <maxim.scherbak@gmail.com>
2020-01-12 13:24:57 +0000

    #35 typo in test_script.sh is fixed

--- test_script.sh
+++ test_script.sh
@@ -2,11 +2,11 @@
 
 case "$1" in
   "TF1")
-    pip intstall -r requirements_tf1.txt
+    pip install -r requirements_tf1.txt
     python -m pytest
   ;;
   "TF2")
-    pip intstall -r requirements_tf2.txt
+    pip install -r requirements_tf2.txt
     python -m pytest
   ;;
 esac
