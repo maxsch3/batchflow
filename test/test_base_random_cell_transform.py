34b12e5eb90fbaa869260ba12efe3a39261cb097
maxsch3 <maxim.scherbak@gmail.com>
2020-03-13 16:29:37 +0000

    #61 added printing actual counts before failing chi square test

--- test/test_base_random_cell_transform.py
+++ test/test_base_random_cell_transform.py
@@ -80,8 +80,12 @@ class TestFeatureDropout:
         assert mask.shape == (data.shape[0], len(cols))
         # check if it is a proper one-hot encoding
         assert mask.sum() == data.shape[0]
+        expected_counts = [5350, 2900, 1650]
         # the counts do not make counts ideally to expected 5000, 3000, 2000
-        c, p = chisquare(mask.sum(0), [5350, 2900, 1650])
+        c, p = chisquare(mask.sum(0), expected_counts)
+        if p > 0.001:
+            print(f'Error. looks like the column distribution {mask.sum(0)} is too far from expected '
+                  f'{expected_counts}')
         assert p > 0.001
 
     def test_zero_mask(self):
