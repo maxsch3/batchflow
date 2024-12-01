05ac79f262c9354e754f1e859cd3326470dfd32c
maxsch3 <maxim.scherbak@gmail.com>
2024-12-01 08:06:55 +0000

    Batch shaper now returns x and y as tuples if multiple

--- tests/batch_transformers/test_shuffle_noise.py
+++ tests/batch_transformers/test_shuffle_noise.py
@@ -1,7 +1,7 @@
 import pytest
 import pandas as pd
 import numpy as np
-from scipy.stats import binom_test, chisquare
+from scipy.stats import binomtest, chisquare
 from keras_batchflow.base.batch_transformers import ShuffleNoise
 
 
